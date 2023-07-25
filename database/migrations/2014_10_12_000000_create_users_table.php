a85b906805a9557975a479908c913b2f81ba9ffd
Asmae Hariti <haritiasmaa74@gmail.com>
2023-07-25 20:34:32 +0100

    coming back to the default case of auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('usertype')->default(0);
-            $table->string('phone')->nullable();
-            $table->string('adress')->nullable();
+           
 
 
             $table->timestamp('email_verified_at')->nullable();
